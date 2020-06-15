c247e6fbb285eab977a9e7d22988ab1faeba5884
Yvana Wagter <yvana@wagter.nl>
2020-06-15 16:50:24 +0200

    dynamic inserts with pdo

--- PHP-practicioner/core/database/QueryBuilder.php
+++ PHP-practicioner/core/database/QueryBuilder.php
@@ -16,13 +16,22 @@ class QueryBuilder
         $results = $statement->fetchAll(PDO::FETCH_CLASS);
         return $results;
      }
+     public function insert($table, $parameters)
+     {
+        $sql = sprintf(
+            'insert into %s (%s) values (%s)',
+            $table,
+            implode(", ", array_keys($parameters)),
+            ":" . implode(", :", array_keys($parameters))
+            
+        );
+        try {
+            $statement = $this->pdo->prepare($sql);
+        
+            $statement->execute($parameters);
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+        
+     }
 }
-
-//other way to do it
-// public function selectAll($table, $class)
-// {
-//    $statement = $this->pdo->prepare("select * from {$table}");
-//    $statement->execute();
-//    $results = $statement->fetchAll(PDO::FETCH_CLASS, $class);
-//    return $results;
-// }
--- PHP-practicioner/views/index.view.php
+++ PHP-practicioner/views/index.view.php
@@ -1,15 +1,17 @@
 <?php require("partials/head.php"); ?>
+<?php require("partials/nav.php"); ?>
 
-    <?php require("partials/nav.php"); ?>
-    <ul>
-    <?php foreach ($tasks as $task) : ?>
-        <li>
-            <?php if ($task->completed) : ?>
-                <strike><?= $task->description; ?></strike>
-            <?php else: ?>
-                <?= $task->description; ?>
-            <?php endif; ?>
-        </li>
-    <?php endforeach; ?>
+    <h1>All the users on here:</h1>
+    <ul> 
+        <?php foreach ($users as $user): ?>
+            <li><?= $user->name ;?></li>
+        <?php endforeach; ?>
     </ul>
+   <h1>Submit your name</h1>
+
+   <form method='POST' action="/names">
+        <input name="name"></input>
+        <button type="submit">Submit</button>
+   </form>
+
 <?php require("partials/footer.php"); ?>
