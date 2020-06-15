c3b95c17085056c39c5d91cef7aef17733c0c21b
Yvana Wagter <yvana@wagter.nl>
2020-06-15 17:30:53 +0200

    added composer

--- PHP-practicioner/core/bootstrap.php
+++ PHP-practicioner/core/bootstrap.php
@@ -3,10 +3,6 @@
 $app = [];
 
 $app["config"] = require "config/config.php";
-require "core/database/Connection.php";
-require "core/database/QueryBuilder.php";
-require "core/router.php";
-require "core/Request.php";
 
 $app["database"] = new QueryBuilder(
     Connection::make($app["config"]["database"])
